f962d59abef1103871a2233542ee3a0351a9b15f
Priya <sreepdsy1994@gmail.com>
2022-03-24 12:15:50 -0400

    Update package

--- interviewQuestionsPractice/ReplaceNumberByLetter.java
+++ interviewQuestionsPractice/ReplaceNumberByLetter.java
@@ -1,9 +1,34 @@
 package interviewQuestionsPractice;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class ReplaceNumberByLetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+//		//ASCII values for A to Z ranges from 65 to 90
+//		for (char ch = 'A'; ch <= 'Z'; ch++) {
+//			int i = ch-65;
+//			System.out.print(ch+":"+i + " ");//prints A=0 B=1 .... X=25
+//		}
+
+		Random randomInt = new Random();
+		int[] intArray = new int[10];
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = randomInt.nextInt(100);
+		}
+
+		System.out.println("Integer array elements : ");
+		Arrays.stream(intArray).forEach(num -> System.out.print(num + " "));
+
+		System.out.println("\nArray elements replaced with alphabet");
+		Arrays.stream(intArray).forEach(num -> System.out.print((char) (num % 26 + 65) + " "));
+
+//		for (int num : intArray) {
+//			char ch = (char)(num%26 + 65);
+//			System.out.print(ch + " ");
+//		}
 
 	}
 
